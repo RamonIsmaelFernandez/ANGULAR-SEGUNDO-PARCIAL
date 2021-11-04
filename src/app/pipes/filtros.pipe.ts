fc0455d380c7da2e00d392410726061c2010ed3c
ismael <lilitofernandez@hotmail.com>
2021-11-03 21:03:21 -0300

    sss

--- src/app/pipes/filtros.pipe.ts
+++ src/app/pipes/filtros.pipe.ts
@@ -7,16 +7,15 @@ export class FiltrosPipe implements PipeTransform {
 
   transform(value: any, arg: any): any {
 
-    /* compruebo que si el argumento esta vacio o es menor a 3*/
+    /*compruebo que si el argumento esta vacio o es menor a 3*/
 
-    /*if ( arg.length < 2) return value;*/
-    
+   if (arg === '' || arg.length < 3) return value;
     const resultPosts = [];
 
     /*compruebo que coincida lo que se escribe con el argumento*/
     
     for (const post of value) {
-      if (post.title.indexOf(arg) > -1) {
+      if (post.title.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
         resultPosts.push(post);
       };
     };
